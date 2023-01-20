9b8e56fe69c001fa97f2dea1e4cc62c007e34f59
SuiCxDe <nitroxcyanide@gmail.com>
2023-01-20 23:17:26 +0530

    add contextProvider

--- src/index.js
+++ src/index.js
@@ -4,7 +4,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 import App from './App';
+import {ContextProvider} from "./contexts/ContextProvider";
+
 
 ReactDOM.render(
-    <App/>, document.getElementById('root')
+    <ContextProvider><App/></ContextProvider>,
+    document.getElementById('root')
 );
